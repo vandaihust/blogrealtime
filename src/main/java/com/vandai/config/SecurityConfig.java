37619e1fa16278bf7d537f839c4b71fed3099511
Văn Đại <minichu1103@gmail.com>
2022-03-03 16:30:05 +0700

    openAPI instead swagger

--- src/main/java/com/vandai/config/SecurityConfig.java
+++ src/main/java/com/vandai/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -61,6 +62,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.authenticated();
 		http.addFilterAfter(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
+	public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui/**",
+                                   "/webjars/**");
+    }
 
 //	@Override
 //	@Bean
---
+++ src/main/java/com/vandai/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.vandai.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("BlogRealtime Api").version(appVersion)
+						.license(new License().name("Apache 1.0").url("http://springdoc.org")));
+	}
+	
+}
